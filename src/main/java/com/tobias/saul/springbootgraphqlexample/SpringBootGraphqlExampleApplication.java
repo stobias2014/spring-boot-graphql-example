34ff2e924d70c7c03dbcbc2d3544901a66852006
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-18 19:49:16 -0500

    added getFoods and saveFood mutation

--- src/main/java/com/tobias/saul/springbootgraphqlexample/SpringBootGraphqlExampleApplication.java
+++ src/main/java/com/tobias/saul/springbootgraphqlexample/SpringBootGraphqlExampleApplication.java
@@ -1,13 +1,32 @@
 package com.tobias.saul.springbootgraphqlexample;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tobias.saul.springbootgraphqlexample.entity.Food;
+import com.tobias.saul.springbootgraphqlexample.service.FoodService;
 
 @SpringBootApplication
 public class SpringBootGraphqlExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGraphqlExampleApplication.class, args);
+	}
+	
+	@Bean
+	ApplicationRunner init(FoodService foodService) {
+		return args -> {
+			Stream.of("Pizza", "Spam", "Eggs", "Avocado").forEach(name -> {
+				Food food = new Food();
+				food.setName(name);
+				foodService.save(food);
+			});
+			foodService.getFoods().forEach(System.out::println);
+		};
 	}
 
 }
--- src/main/java/com/tobias/saul/springbootgraphqlexample/service/FoodService.java
+++ src/main/java/com/tobias/saul/springbootgraphqlexample/service/FoodService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 import com.tobias.saul.springbootgraphqlexample.entity.Food;
 import com.tobias.saul.springbootgraphqlexample.repository.FoodRepository;
 
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
@@ -23,8 +25,13 @@ public class FoodService {
 	}
 	
 	@GraphQLQuery(name = "foods")
-	public List<Food> getAll() {
+	public List<Food> getFoods() {
 		return foodRepository.findAll();
+	}
+	
+	@GraphQLMutation(name = "saveFood")
+	public Food save(@GraphQLArgument(name = "food") Food food) {
+		return foodRepository.save(food);
 	}
 
 }
